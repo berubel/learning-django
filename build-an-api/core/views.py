49bd0e07b79b3e736f74bfe5821d39c5beef17df
Gabriele Cardoso das Virgens <gabrielecardosov@gmail.com>
2022-04-27 16:24:35 -0300

    Added basic API

---
+++ build-an-api/core/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+# third party imports
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+# An API view is something that we can inherit in our own classes 
+'''
+class TestView(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {
+            'name': 'john',
+            'age': 23
+        }
+        return Response(data)
+# Create your views here.
+'''
+def test_view(request): # That is a dictionary
+    data = {
+        'name': 'john',
+        'age': 23
+    }
+    return JsonResponse(data)
+
+    # By default the JSON response only returns a dictionary, but
+    # you can pass a list using the safe argument, likes "safe=False"
+    # That allows you to return a list.
+    # return JsonResponse(data)

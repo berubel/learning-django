5b9b5e93cde55135da2e0f0cfbbd6340b33a631d
Gabriele Cardoso das Virgens <gabrielecardosov@gmail.com>
2022-04-28 15:24:58 -0300

    Added Django Rest framework

--- build-an-api/core/views.py
+++ build-an-api/core/views.py
@@ -6,7 +6,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 # An API view is something that we can inherit in our own classes 
-'''
+
 class TestView(APIView):
     def get(self, request, *args, **kwargs):
         data = {
@@ -15,13 +15,13 @@ class TestView(APIView):
         }
         return Response(data)
 # Create your views here.
-'''
-def test_view(request): # That is a dictionary
-    data = {
-        'name': 'john',
-        'age': 23
-    }
-    return JsonResponse(data)
+
+# def test_view(request): # That is a dictionary then is converted in a Json payload
+#    data = {
+#        'name': 'john',
+#        'age': 23
+#    }
+#    return JsonResponse(data)
 
     # By default the JSON response only returns a dictionary, but
     # you can pass a list using the safe argument, likes "safe=False"
--- build-an-api/src/urls.py
+++ build-an-api/src/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from core.views import test_view
+from core.views import TestView #, test_view
 
 urlpatterns = [
+    path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('', test_view, name='test'), # we have to say as view because it's a class space
-    path('api-auth/', include('rest_framework.urls'))
+    path('', TestView.as_view(), name='test'), # we have to say as view because it's a class space
 ]

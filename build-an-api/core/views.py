d26d23a11d87baea2ea0bf9470754df1739bea49
Gabriele Cardoso das Virgens <gabrielecardosov@gmail.com>
2022-04-30 14:37:38 -0300

    Working with views: PostCreateView and ListPostCreateView

--- build-an-api/core/views.py
+++ build-an-api/core/views.py
@@ -21,6 +21,34 @@ from core import serializers
 
 # An API view is something that we can inherit in our own classes 
 
+class TestView(APIView):
+    # permition_classes is a property we get acess to when we inherit from APIView and other rappers
+    # Is authenticated means no matter wich method were sending as a request method so
+    # wether it's get or post we will be authenticate in order for this view, otherwise we'll just
+    # get an authenticated error.
+    # permission_classes = (IsAuthenticated, )
+
+    def get(self, request, *args, **kwargs):
+        # Return a list of the posts
+        # Using a serializer when we retrieved a data
+        qs = Post.objects.all()
+        # post =  qs.first() # returning the first instance in the database
+        serializer = PostSerializer(qs, many=True) # returning many instances (list of dictionaries)
+        # serializer = PostSerializer(post) # returning one instance (a dictionary)
+        return Response(serializer.data)
+
+    # Using a serializer when we received a data
+
+    # In post we make sure that data we're receiving 
+    # is valid especially when we want to manipulate the database
+
+    def post(self, request, *args, **kwargs): 
+        serializer = PostSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save() # save a instance of a model
+            return Response(serializer.data)
+        return Response(serializer.errors) 
+
 # Mixins 
 # They are basically containers of functionality that you can include in your classes
 # to inherit off of that provide some sort of functionality. Always include them at the beggining of
@@ -50,6 +78,23 @@ class PostView(
         # send an email
         serializer.save()
         #return super().perform_create(serializer)
+    
+# Does the same thing of the PostView class
+# But u dont need to especify methods cuz them are inherited from CreteAPIView that's inheriting of GenericAPIView
+# Otherwise this class dont handles the get method, for use it u can inherit ListModelMixin and override it
+
+class PostCreateView(mixins.ListModelMixin, generics.CreateAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+    def get(self, request, *args, **kwargs):
+            return self.list(request, *args, **kwargs)
+            
+# Exactly same functionality as the previous ones
+class PostListCreateView(generics.ListCreateAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
 
 
 """ class TestView(APIView):

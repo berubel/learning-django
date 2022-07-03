595fc7cf9590cd9b18427219c46169d4c0548814
Gabriele Cardoso das Virgens <gabrielecardosov@gmail.com>
2022-07-03 20:51:40 -0300

    Created table 'Evento' to the Schedule project

--- db-django/schedule/core/admin.py
+++ db-django/schedule/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from core.models import Evento
 
 # Register your models here.
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento', 'data_criacao') #shows the fields we want to see
+    list_filter = ('usuario','data_evento') # add a filter to search
+
+admin.site.register(Evento, EventoAdmin)
--- db-django/schedule/core/models.py
+++ db-django/schedule/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True)
+    data_evento = models.DateTimeField(verbose_name='data do evento')
+    data_criacao = models.DateTimeField(auto_now=True, verbose_name= 'data da criação')
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE) # on_delete parameter tells django
+                                                                  # what to do if user is deleted
+
+    class Meta:
+        db_table = 'evento'
+
+    def __str__(self):
+        return self.titulo

60fc1e38dff8948bac7bd3b2f7e9cd733413832b
Gabriele Cardoso das Virgens <gabrielecardosov@gmail.com>
2022-08-26 17:50:52 -0300

    Created task_delete view

--- to-do-CRUD/api/urls.py
+++ to-do-CRUD/api/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('task-detail/<str:id>/', views.task_detail, name='task-detail'),
     path('task-create/', views.task_create, name='task-create'),
     path('task-update/<str:id>/', views.task_update, name='task-update'),
+    path('task-delete/<str:id>', views.task_delete, name='task-delete')
 ]
--- to-do-CRUD/api/views.py
+++ to-do-CRUD/api/views.py
@@ -22,12 +22,14 @@ def api_overview(request):
 def task_list(request):
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many=True)
+
     return Response(serializer.data)
 
 @api_view(['GET'])
 def task_detail(request, id):
     tasks = Task.objects.get(id=id)
     serializer = TaskSerializer(tasks, many=False)
+
     return Response(serializer.data)
 
 @api_view(['POST'])
@@ -48,4 +50,10 @@ def task_update(request, id):
         serializer.save()
 
     return Response(serializer.data)
-    
+
+@api_view(['DELETE'])
+def task_delete(request, id):
+    task = Task.objects.get(id=id)
+    task.delete()
+
+    return Response('Task sucefully delete!')

ae0c48218b3225153a069400bba45f387a19c229
Gabriele Cardoso das Virgens <gabrielecardosov@gmail.com>
2022-08-26 15:41:00 -0300

    Created task_list view

--- to-do-CRUD/api/urls.py
+++ to-do-CRUD/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.api_overview, name="api-overview")
+    path('', views.api_overview, name="api-overview"),
+    path('task-list/', views.task_list, name='task-list'),
 ]
--- to-do-CRUD/api/views.py
+++ to-do-CRUD/api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import TaskSerializer
+from .models import Task
 
 # Create your views here.
 
@@ -16,3 +18,8 @@ def api_overview(request):
     }
     return Response(api_urls)
 
+@api_view(['GET'])
+def task_list(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
